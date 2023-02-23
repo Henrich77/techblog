645cc2a925aa9418654842e7db54b3e9822fd0f5
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-02-23 17:50:00 -0600

    fixed the model and routed for the blogs

--- models/Blog.js
+++ models/Blog.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Blog extends Model {}
 
-Project.init(
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,7 +11,7 @@ Project.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -22,10 +22,6 @@ Project.init(
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,
@@ -44,4 +40,4 @@ Project.init(
   }
 );
 
-module.exports = Project;
+module.exports = Blog;
