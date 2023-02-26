4f6d8443cc094963332fe5c55da0c85eea7c93c0
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-02-26 11:53:15 -0600

    modified routes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs({defaultLayout:'main'}))
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
