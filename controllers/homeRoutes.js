1d79783a9481af1f9f36ff72f9994a4883ec4407
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-02-27 14:32:10 -0600

    updated and fixed the index  hanldebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,29 +2,29 @@ const router = require('express').Router();
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => { res.render(`index`)
-  // try {
-    // Get all posts and JOIN with user data
-  //   const blogData = await Blog.findAll({
-  //     include: [
-  //       {
-  //         model: User,
-  //         attributes: ['name'],
-  //       },
-  //     ],
-  //   });
+router.get('/', async(req, res) => { 
+  try {
+  
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-  //   // Serialize data so the template can read it
-  //   const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-  //   // Pass serialized data and session flag into template
-  //   res.render('main', { 
-  //     blogs, 
-  //     logged_in: req.session.logged_in 
-  //   });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+    // Pass serialized data and session flag into template
+    res.render('index', { 
+      blogs, 
+      // logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/blog/:id', async (req, res) => {
